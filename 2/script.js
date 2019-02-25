04bd0be150c8b3d137b225dd7f73ab5b4c1b415d
Dima Sazonov <mister.mitya99@gmail.com>
2019-02-25 22:02:33 +0300

    fixed second task

--- 2/script.js
+++ 2/script.js
@@ -1,20 +1,18 @@
-let number = document.getElementsByTagName('input')[0],
-  buttonClick = document.querySelector('#clickButton'),
-  wrap = document.querySelector('.wrap');
+const number = document.getElementsByTagName('input')[0];
+const buttonClick = document.querySelector('#clickButton');
+const wrap = document.querySelector('.wrap');
 
 function getRandomColor() {
-
   let letters = '0123456789ABCDEF'.split(''),
     color = '#';
 
   for (let i = 0; i < 6; i++) {
-
     color += letters[Math.round(Math.random() * 15)];
   }
   return color;
 }
 
-buttonClick.addEventListener('click', function () {
+buttonClick.addEventListener('click', function() {
 
   if (number.value < 31 && number.value > -1) {
 
@@ -24,10 +22,10 @@ buttonClick.addEventListener('click', function () {
 
         let lilDiv = document.createElement('div');
         lilDiv.classList.add('wraperblock');
-        lilDiv.style.backgroundColor = getRandomColor()
+        lilDiv.style.backgroundColor = getRandomColor();
         wrap.appendChild(lilDiv);
       }
-      let br = document.createElement('br');
+      const br = document.createElement('br');
       wrap.appendChild(br);
     }
   } else alert('Введите число от 0 до 30');
