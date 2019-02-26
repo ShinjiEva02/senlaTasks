f8f82639a35ea253d6d589dcc63cb420a6f7bcbe
Dima Sazonov <mister.mitya99@gmail.com>
2019-02-26 21:57:34 +0300

    fixed third task

--- 3/script.js
+++ 3/script.js
@@ -1,20 +1,18 @@
 const number = document.getElementsByTagName('input')[0];
 const buttonClick = document.querySelector('#clickButton');
 const p = document.createElement('p');
-let arr = [],
-  a = '';
-
+let arr, shortWord;
 
 buttonClick.addEventListener('click', function() {
 
   arr = number.value.split(' ');
-  a = arr[0].length;
+  shortWord = arr[0].length;
   for (let i = 0; i < arr.length; i++) {
 
-    if (a > arr[i].length) {
-      a = arr[i].length;
+    if (shortWord > arr[i].length) {
+      shortWord = arr[i].length;
     }
   }
-  p.innerHTML = `The shortest length of the word: ${a}`;
+  p.innerHTML = `The shortest length of the word: ${shortWord}`;
   document.body.appendChild(p);
 });
