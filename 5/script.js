d97099a25f70a7f039435047a16925399c428eac
Dima Sazonov <mister.mitya99@gmail.com>
2019-02-26 21:49:44 +0300

    fixed fifth task

--- 5/script.js
+++ 5/script.js
@@ -1,11 +1,16 @@
 function getLowerestIndex(arr, sup) {
   let arw = arr.sort();
 
-  for (let i = 0; i < arw.length; i++) {
-
-    if (sup > arw[i] && sup < arw[i + 1]) {
-      return i + 1;
+  if (arw[0] > sup) return 0
+  else if (arw[arw.length - 1] < sup) return arw.length
+  else if (arw.indexOf(sup) || sup == arw[0]) return 'This element already exists in array'
+  else {
+    for (let i = 0; i < arw.length; i++) {
+      if (sup > arw[i] && sup < arw[i + 1]) {
+        return i + 1;
+      }
     }
   }
 }
-console.log(getLowerestIndex([2, 3, 1, 4], 1.5));
+
+console.log(getLowerestIndex([2, 3, 5, 4], 2));
